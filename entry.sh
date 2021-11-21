66013abff4209017a87a3711b03f7b4dc0e46143
Kiriakos Velissariou <kiriakosv@users.noreply.github.com>
2021-11-21 16:56:32 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ `git status --porcelain` ]]; then
-  git config --global user.name "The Bot"
+  git config --global user.name "The Bot 🤖"
   git config --global user.email "the-bot@example.com"
   git add .
   git commit -am "Update content" || exit 0
